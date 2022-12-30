3a5c610b18faf134a7fd9731fca59519a5e9b6eb
Numed <ruschinets09@gmail.com>
2022-12-30 21:59:25 +0200

    add a smooth scroll

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,16 +3,32 @@ import HeaderSection from "../headerSection/HeaderSection";
 import FooterSection from "../footerSection/FooterSection";
 import ImgSection from "../imgSection/ImgSection";
 import AboutUs from "../aboutUs/AboutUs";
+import { gsap } from "gsap-trial";
+import ScrollTrigger from "gsap-trial/ScrollTrigger";
+import ScrollSmoother from "gsap-trial/ScrollSmoother";
+import { useEffect, useRef } from "react";
 
 const App = () => {
+  const el = useRef();
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  gsap.utils.selector(el);
+  useEffect(() => {
+    ScrollSmoother.create({
+      smooth: 2,
+      effects: true,
+      smoothTouch: 0.1,
+    });
+  }, []);
   return (
-    <>
-      <HeaderSection />
-      <BannerSection />
-      <AboutUs />
-      <ImgSection />
-      <FooterSection />
-    </>
+    <div ref={el} id="smooth-wrapper">
+      <div id="smooth-content">
+        <HeaderSection />
+        <BannerSection />
+        <AboutUs />
+        <ImgSection />
+        <FooterSection />
+      </div>
+    </div>
   );
 };
 
